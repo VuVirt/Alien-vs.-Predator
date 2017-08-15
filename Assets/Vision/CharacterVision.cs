b1d8b0c42c06c3e31d4f2bc9f3712b787fab3dd4
vuvirt <ivo.kachie@gmail.com>
2017-08-16 00:42:08 +0300

    Character Vision script
    A C# script that will allow the designers to control the character Emission for each mode. The cameras will use the an object of this type for each character in order to configure the character rendering for each mode.

---
+++ Assets/Vision/CharacterVision.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[AddComponentMenu("Vision/CharacterVision")]
+
+internal enum VisionMode
+{
+    None,
+    Thermal, 
+    Night,
+    EM
+}
+public class CharacterVision : MonoBehaviour {
+    // public string materialEmissionProperty = "_EmissionColor";
+
+    [Range(0.0f, 2.0f)] public float ThermalEmission = 1.0f;
+    [Range(0.0f, 2.0f)] public float NightEmission = 1.0f;
+    [Range(0.0f, 2.0f)] public float EMEmission = 1.0f;
+
+    void Start()
+    {
+        Apply(VisionMode.None);
+    }
+
+    void Update()
+    {
+    }
+
+    internal void Apply(VisionMode mode)
+    {
+        Renderer[] rn = GetComponentsInChildren<Renderer>();
+        foreach (Renderer r in rn)
+        {
+            switch(mode)
+            {
+                case VisionMode.Thermal:
+                    r.sharedMaterial.SetColor("_EmissionColor", Color.white * ThermalEmission);
+                    break;
+
+                case VisionMode.Night:
+                    r.sharedMaterial.SetColor("_EmissionColor", Color.white * NightEmission);
+                    break;
+
+                case VisionMode.EM:
+                    r.sharedMaterial.SetColor("_EmissionColor", Color.white * EMEmission);
+                    break;
+
+                default:
+                    r.sharedMaterial.SetColor("_EmissionColor", Color.black);
+                    break;
+            }
+        }
+    }
+}
